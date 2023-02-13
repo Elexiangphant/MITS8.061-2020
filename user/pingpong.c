da68b2185eb6ecbb797a45bf5beeaf667b3b2979
yixia-vm <1257597350@qq.com>
2023-02-13 10:58:19 +0800

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,16 +3,24 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
+	int pp[2],pc[2];
 	
+	pipe(pp);
+	pipe(pc);
 	
 	if(fork() != 0) { 
-		
-		wait(0);
+		char buf;
+		write(pp[1],"!",1);
+		read(pc[0],&buf,1);
 		printf("%d: received pong\n", getpid()); 
+		wait(0);
 		
 	} else { 
+		char buff;
+		read(pp[0],&buff,1);
 		
 		printf("%d: received ping\n", getpid());
+		write(pc[1],&buff,1);
 		
 	}
 	exit(0);
